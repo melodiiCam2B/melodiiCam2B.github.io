5b7a16645df3228c14b4347dbf991abc2f2749e1
Melodii2b <115182751+melodiiCam2B@users.noreply.github.com>
2025-06-04 20:35:28 +0200

    graw!

---
+++ src/functions/buttons.js
@@ -0,0 +1,10 @@
+function clickMusicB(){
+    var m = document.getElementById('alorsondanse');
+    m.play();
+}
+function goHome(){
+      location.replace("https://melodiicam2b.github.io")
+}
+function getLink(){
+    location.replace("https://melodiicam2b.github.io/404")
+}
