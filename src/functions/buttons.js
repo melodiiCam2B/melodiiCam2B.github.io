93872674430edeb2f0489c203a455a197d60a48b
Melodii2b <115182751+melodiiCam2B@users.noreply.github.com>
2025-06-05 18:54:42 +0200

    hgsdika

--- src/functions/buttons.js
+++ src/functions/buttons.js
@@ -2,11 +2,12 @@ function clickMusicB(){
     var m = document.getElementById('alorsondanse');
     m.play();
 }
-function getLink(link){
-    switch(link){
-        case'lonk':location.replace("https://melodiicam2b.github.io/links");
-        case'home':location.replace("https://melodiicam2b.github.io");
-        case'boho':location.replace("https://melodiicam2b.github.io/boohoo");
-        default:location.replace("https://melodiicam2b.github.io/404");
-    }
+function goHome(){
+    location.replace("https://melodiicam2b.github.io");
+}
+function goLink(){
+    location.replace("https://melodiicam2b.github.io/404");
+}
+function gofren(){
+    location.replace("https://melodiicam2b.github.io/boohoo");
 }
