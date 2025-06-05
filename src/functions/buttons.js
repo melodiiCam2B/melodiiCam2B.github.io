b67d341b1f671055a45c9649edfdec6e6e260eca
Melodii2b <115182751+melodiiCam2B@users.noreply.github.com>
2025-06-05 18:50:21 +0200

    rahg!

--- src/functions/buttons.js
+++ src/functions/buttons.js
@@ -2,12 +2,11 @@ function clickMusicB(){
     var m = document.getElementById('alorsondanse');
     m.play();
 }
-function goHome(){
-      location.replace("https://melodiicam2b.github.io");
-}
-function getLink(){
-    location.replace("https://melodiicam2b.github.io/404");
-}
-function getfren(){
-    location.replace("https://melodiicam2b.github.io/boohoo");
+function getLink(link){
+    switch(link){
+        case'lonk':location.replace("https://melodiicam2b.github.io/links");
+        case'home':location.replace("https://melodiicam2b.github.io");
+        case'boho':location.replace("https://melodiicam2b.github.io/boohoo");
+        default:location.replace("https://melodiicam2b.github.io/404");
+    }
 }
