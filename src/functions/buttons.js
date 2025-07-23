aecbca8f056061b22847aff335510f764fb40bf5
Melodii2b <115182751+melodiiCam2B@users.noreply.github.com>
2025-07-23 14:47:02 +0200

    whoopy

--- src/functions/buttons.js
+++ src/functions/buttons.js
@@ -6,17 +6,16 @@ function gotHome(){
     location.replace("https://melodiicam2b.github.io");
 }
 function getLink(){
-    location.replace("https://melodiicam2b.github.io/404");
+    location.replace("https://melodiicam2b.github.io/404.html");
 }
 function getfren(){
-    location.replace("https://melodiicam2b.github.io/boohoo");
+    location.replace("https://melodiicam2b.github.io/boohoo.html");
 }
 function gethero(){
-    location.replace("https://melodiicam2b.github.io/hero");
+    location.replace("https://melodiicam2b.github.io/hero.html");
 }
 
-// function button(action, sound){
-//     var m = document.getElementById(sound?? sound : 'placeholder');
-//     m.play();
-
-// }
+function button(action, sound) {
+    var m = document.getElementById(sound ?? 'placeholder');
+    m.play();
+}
