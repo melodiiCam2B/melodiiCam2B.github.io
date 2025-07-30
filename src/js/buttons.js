452fac8e2e3e23bc68adbcd4adc29859d0968bf7
Melodii2b <115182751+melodiiCam2B@users.noreply.github.com>
2025-07-30 14:49:56 +0200

    WEWE

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,8 +1,4 @@
-function toSite(id){
-    switch(id){
-        case'start':location.replace("https://melodiicam2b.github.io");
-        case'git':location.replace("https://github.com/melodiiCam2B/melodiiCam2B.github.io");
-        default:location.replace("https://melodiicam2b.github.io/404");
-    }
+function toSite(d){
+    location.replace("https://github.com/melodiiCam2B/melodiiCam2B.github.io");
 
 }
