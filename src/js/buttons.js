630f408dc20d4c4a10625c10db385f7c28c71756
Melodii2b <115182751+melodiiCam2B@users.noreply.github.com>
2025-07-30 14:55:30 +0200

    test

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,4 +1,8 @@
-function toSite(){
-    location ="https://github.com/melodiiCam2B/melodiiCam2B.github.io";
+function toSite(id){
+    switch(id){
+        case'start':location="https://melodiicam2b.github.io";
+        case'git':location="https://github.com/melodiiCam2B/melodiiCam2B.github.io";
+        default:location="https://melodiicam2b.github.io/404";
+    }
 
 }
