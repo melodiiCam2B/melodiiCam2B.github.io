dba33a60ac9192ab8da86df6ba8d33e267e0cb25
Melodii2b <115182751+melodiiCam2B@users.noreply.github.com>
2025-08-27 14:35:48 +0200

    plush

--- .archive/src/js/buttons.js
+++
@@ -1,8 +0,0 @@
-function toSite(id){
-    switch(id){
-        case'start':location="https://melodiicam2b.github.io";
-        case'git':location="https://github.com/melodiiCam2B/melodiiCam2B.github.io";
-        default:location="https://melodiicam2b.github.io/404";
-    }
-
-}
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Modal from './Modal.vue'
+import { ref } from 'vue'
+
+const showModal = ref(false)
+</script>
+
+<template>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+  <Teleport to="body">
+    <!-- use the modal component, pass in the prop -->
+    <modal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>Custom Header</h3>
+      </template>
+    </modal>
+  </Teleport>
+</template>
